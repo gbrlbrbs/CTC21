73cbbd4e4de437f84e3c7166108830b8ddfc4022
gbrlbrbs <gabrielbarbosamartinz@gmail.com>
2021-06-25 14:11:26 -0300

    start answering

--- src/CTC21.jl
+++ src/CTC21.jl
@@ -1,11 +1,11 @@
 module CTC21
-using Base: func_for_method_checked
-export greet
+import BenchmarkTools
+export greet, euclid, extended_euclid, fermat_factor
 
 greet() = print("Hello World!")
 
 function euclid(a::Int64, b::Int64)::Int64
-    
+
     if b > a
         tmp = a
         a = b
@@ -18,7 +18,7 @@ function euclid(a::Int64, b::Int64)::Int64
         a = tmp
     end
 
-    return b
+    return a
 end
 
 """
@@ -31,7 +31,7 @@ if `c` is divisible by `gcd(a, b)`. If `c` is not divisible, then it returns [Na
 function extended_euclid(a::Int64, b::Int64, c::Int64)
     x, s = 1, 0
     y, t = 0, 1
-    
+
     if b > a
         tmp = a
         a = b
@@ -53,6 +53,17 @@ function extended_euclid(a::Int64, b::Int64, c::Int64)
             tmp = t; t = y - q*s; y = tmp
         end
         return [gcd, x, y]
+    end
+end
+
+function fermat_factor(N::Int64)
+    a = ceil(Int64, sqrt(N))
+    b = sqrt(a^2 - N)
+    while floor(Int64, b) != ceil(Int64, b)
+        a += 1
+        b = sqrt(a^2 - N)
+    end
+    return [a - b, a + b]
 end
 
 end # module
